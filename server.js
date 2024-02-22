5eff02f16fc53f213fdca699085efced076e9e77
EasySimho <s.benanchietti.yt@gmail.com>
2024-02-23 00:05:51 +0100

    new style

--- server.js
+++ server.js
@@ -2,21 +2,33 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/chat', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const messageSchema = new mongoose.Schema({
+  message: String
+});
+
+const Message = mongoose.model('Message', messageSchema);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  Message.find().then(messages => {
+    socket.emit('load messages', messages);
   });
+
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+    const message = new Message({ message: msg });
+    message.save().then(() => {
+      io.emit('chat message', msg);
+    });
   });
 });
 
-server.listen(process.env.PORT || 433, () => {
-  console.log('listening on: ', process.env.PORT);
+server.listen(process.env.PORT || 3000, () => {
+  console.log('listening on *:' + (process.env.PORT || 3000));
 });
